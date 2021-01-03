d22077aa01635027b7c876b5870a44119ba30c51
Kat Leight <leightkt@gmail.com>
2021-01-02 17:05:26 -0700

    commit tree d627d0031122f4f1e72b419c6698f8b77dd7a6fe

--- app/models/Show.rb
+++ app/models/Show.rb
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
     end
 
     def self.least_popular_show
-        Show.where("rating = ?", Show.lowest_rating).first
+        Show.find_by(rating: Show.lowest_rating)
     end
 
     def self.ratings_sum
